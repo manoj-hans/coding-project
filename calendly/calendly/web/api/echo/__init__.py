ba427ad26cf691087f94b1f9bacf36b610e945e5
Manoj Hans <manojhans1989@gmail.com>
2024-06-18 22:33:06 +0530

    boilerplate code added

---
+++ calendly/calendly/web/api/echo/__init__.py
@@ -0,0 +1,4 @@
+"""Echo API."""
+from calendly.web.api.echo.views import router
+
+__all__ = ["router"]
---
+++ calendly/calendly/web/application.py
@@ -0,0 +1,34 @@
+from importlib import metadata
+
+from fastapi import FastAPI
+from fastapi.responses import UJSONResponse
+
+from calendly.web.api.router import api_router
+from calendly.web.lifetime import register_shutdown_event, register_startup_event
+
+
+def get_app() -> FastAPI:
+    """
+    Get FastAPI application.
+
+    This is the main constructor of an application.
+
+    :return: application.
+    """
+    app = FastAPI(
+        title="calendly",
+        version=metadata.version("calendly"),
+        docs_url="/api/docs",
+        redoc_url="/api/redoc",
+        openapi_url="/api/openapi.json",
+        default_response_class=UJSONResponse,
+    )
+
+    # Adds startup and shutdown events.
+    register_startup_event(app)
+    register_shutdown_event(app)
+
+    # Main router for the API.
+    app.include_router(router=api_router, prefix="/api")
+
+    return app
